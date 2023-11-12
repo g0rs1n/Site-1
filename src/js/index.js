86597bbecb354e81b7c765680bd4e01dd436c7a9
Gorsin <NoneEmail@tochka.com>
2023-11-12 12:23:37 +0200

    First Commit

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+import mobileNav from './modules/mobile-nav.js';
+mobileNav();
+
+import Swiper from './modules/swiper-section-2.js';
+Swiper();
---
+++ src/js/modules/swiper-section-2.js
@@ -0,0 +1,9 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 3.1,
+    spaceBetween: 24,
+});
+
+export default Swiper;
