6babe9db549d00403b4afa3f0ef8ba28de0887ac
Gorsin <NoneEmail@tochka.com>
2023-11-19 20:13:00 +0200

    Plus section-4

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,22 @@ mobileNav();
 
 import Swiper from './modules/swiper-section-2.js';
 Swiper();
+
+// const accordeon = document.querySelector('.accordeon');
+// const accordeonTitles = accordeon.querySelectorAll('.accordeon__title');
+
+//     accordeonTitles.forEach.call(accordeonTitles, function (accordeonTitle) {
+//         accordeonTitle.addEvenListener('click', function() {
+
+//             const currentText = accordeonTitle.parentElement.querySelector('.accordeon__text');
+
+//             accordeonTitle.classList.toggle('accordeon__title-active');
+//             currentText.classList.toggle('accordeon__text-visible');
+
+//             if (currentText.classList.conteins('accordeon__text-visible')) {
+//                 currentText.style.maxHeight = currentText.scrollHeight + 'px'
+//             } else {
+//                 currentText.style.maxHeight = null
+//             }
+//         })
+//     });
