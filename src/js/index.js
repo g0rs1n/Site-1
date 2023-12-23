0e1f0fbb347e8bbadcfbcf01d0da67c647d4cdc4
Gorsin <NoneEmail@tochka.com>
2023-12-23 18:43:12 +0200

    plus discover-page

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,10 @@ mobileNav();
 import Swiper from './modules/swiper-section-2.js';
 Swiper();
 
+// import Tabs from './modules/tabs.js';
+// Tabs();
+
+
 // const items = Array.from(document.querySelectorAll(".item"));
     
 // items.forEach((item) => {
@@ -21,3 +25,18 @@ Swiper();
 //         currentContent.style.maxHeight = 0;
 //     }
 // }
+
+
+// const tabsTitle = document.querySelectorAll('.discover-item');
+// const tabsContent = document.querySelectorAll('.discover-cards');
+
+// tabsTitle.forEach(item => item.addEventListener('click', event => {
+//     const tabsTitleTarget = event.target.getAttribute('data-tab');
+//     tabsTitle.forEach(element => element.classList.remove('active-tab'));
+//     tabsContent.forEach(element => element.classList.add('hidden-card-tab'));
+//     item.classList.add('active-tab');
+//     document.getElementByld(tabsTitleTarget).classList.remove('hidden-card-tab');
+// }))
+
+// document.querySelector('[data-tab="tab-1"]').classList.add('active-tab');
+// document.querySelector('#tab-1').classList.remove('hidden-card-tab');
---
+++ src/js/modules/tabs.js
@@ -0,0 +1,21 @@
+// function Tabs(){
+
+// const tabsTitle = document.querySelectorAll('.discover-item');
+// const tabsContent = document.querySelectorAll('.discover-cards');
+
+// tabsTitle.forEach(item => item.addEventListener('click', event => {
+//     const tabsTitleTarget = event.target.getAttribute('data-tab');
+//     tabsTitle.forEach(element => element.classList.remove('active-tab'));
+//     tabsContent.forEach(element => element.classList.add('hidden-card-tab'));
+//     item.classList.add('active-tab');
+//     document.getElementByld(tabsTitleTarget).classList.remove('hidden-card-tab');
+// }))
+
+// document.querySelector('[data-tab="tab-1"]').classList.add('active-tab');
+// document.querySelector('#tab-1').classList.remove('hidden-card-tab');
+
+// } 
+
+
+// export default Tabs;
+
